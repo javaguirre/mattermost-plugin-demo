fa8cd80b602914405454fd28facb68ae3f28f6f6
Javier Aguirre <contacto@javaguirre.net>
2022-10-28 12:54:19 +0200

    v6

--- server/configuration.go
+++ server/configuration.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/pkg/errors"
 
-	"github.com/mattermost/mattermost-server/v5/model"
-	"github.com/mattermost/mattermost-server/v5/plugin"
+	"github.com/mattermost/mattermost-server/v6/model"
+	"github.com/mattermost/mattermost-server/v6/plugin"
 )
 
 // configuration captures the plugin's external configuration as exposed in the Mattermost server
--- server/plugin.go
+++ server/plugin.go
@@ -4,7 +4,7 @@ import (
 	"sync"
 
 	"github.com/mattermost/mattermost-plugin-api/cluster"
-	"github.com/mattermost/mattermost-server/v5/plugin"
+	"github.com/mattermost/mattermost-server/v6/plugin"
 )
 
 type Plugin struct {
